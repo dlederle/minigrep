bd219783109a6ee9f80eed91c3dfc071dba0e4c4
Dylan Lederle-Ensign <dylan.lederle-ensign@viget.com>
2022-05-12 21:34:56 -0400

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ use std::fs;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
     Ok(())
 }
 
@@ -25,4 +24,32 @@ impl Config {
 
         Ok(Config { query, filename })
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
