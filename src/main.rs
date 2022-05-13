bb317c11c2d6a5f24755145d6f43c51f1d3b8a9b
Dylan Lederle-Ensign <dylan.lederle-ensign@viget.com>
2022-05-12 21:00:02 -0400

    Pull out lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,20 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-
-    let config = Config::new(&args).unwrap_or_else( |err| {
+    let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        // Clone makes a full copy of the string.
-        // This trades performance for simplicity
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
